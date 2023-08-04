a60e781ac7dc72bd74d0757048f12537c5aa1955
Welber33 <jwwork21@gmail.com>
2023-08-04 20:56:04 -0300

    Fixing miss spelling issue w/ button style(Store)

--- components/info.tsx
+++ components/info.tsx
@@ -56,7 +56,7 @@ export default function Info({ data }: InfoProps) {
       <div className="mt-10 flex items-center gap-x-3">
         <Button
           onClick={onAddToCart}
-          className="flex items-center gap-x-2 rouded-md"
+          className="flex items-center gap-x-2 rounded-md"
         >
           Add to Cart
           <ShoppingCart />
